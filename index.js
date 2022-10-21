d0751373b425f62fe1785a2b1ce006724d817d8a
SeymaDemir <seymademir35@gmail.com>
2022-10-21 15:39:27 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+const main = document.getElementById('main')
+main.remove();
+
+const newHeader = document.createElement('h1')
+// document.body.append(newHeader)
+
+newHeader.id = 'victory';
+newHeader.textContent = "Şeyma is the champion"
